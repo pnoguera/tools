071c177d9cb0863b9cc12ac48fa69de85fa26a10
Pablo Noguera <pablo.noguera@flixbus.com>
2020-02-17 13:27:56 +0100

    Add gosec

--- tools.go
+++ tools.go
@@ -7,5 +7,6 @@ import (
 	_ "github.com/gobuffalo/buffalo/buffalo"                   // Go Buffalo
 	_ "github.com/monochromegane/the_platinum_searcher/cmd/pt" // platinum searcher
 	_ "github.com/parkghost/gohttpbench"                       // http benchmarking tool
+	_ "github.com/securego/gosec/cmd/gosec"                    // Go code security inpection
 	_ "github.com/tsenart/vegeta"                              // http load tool
 )
